6dc65f848e02a499b580bd2cb1c08933f00be177
David Guzman <d.guzman@ucl.ac.uk>
2021-04-30 13:32:46 +0100

    Refactor rename test

--- twitter-compliance-data/src/test/java/uk/ac/ucl/twitter/compliance/persist/TweetToVerifyWeeklyEntityTest.java
+++ twitter-compliance-data/src/test/java/uk/ac/ucl/twitter/compliance/persist/TweetToVerifyWeeklyEntityTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Unit tests for {@see uk.ac.ucl.twitter.compliance.persist.TweetToValidateWeeklyEntity}
+ * Unit tests for {@see uk.ac.ucl.twitter.compliance.persist.TweetToVerifyWeeklyEntity}
  * based on the recommendations listed in
  * <a href="http://mjremijan.blogspot.com/2016/06/unit-testing-jpa-annotationsstop_20.html">
  * https://mjremijan.blogspot.com/2016/06/unit-testing-jpa-annotationsstop_20.html
@@ -14,11 +14,11 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author David Guzman {@literal d.guzman at ucl.ac.uk}
  *
  */
-class TweetToValidateWeeklyEntityTest {
+class TweetToVerifyWeeklyEntityTest {
 
   @Test
   public void hasEntityAnnotation() {
-    Class<TweetToValidateWeeklyEntity> clEntity = TweetToValidateWeeklyEntity.class;
+    Class<TweetToVerifyWeeklyEntity> clEntity = TweetToVerifyWeeklyEntity.class;
     Entity annotation = clEntity.getAnnotation(Entity.class);
     assertNotNull(annotation, "Expect class to be annotated as Entity");
     assertEquals("", annotation.name(), "Expect name to be empty");
