7cc43ddf001a1b3d97171b7444e81a50c3cb5f36
David Guzman <d.guzman@ucl.ac.uk>
2021-05-04 18:57:37 +0100

    Add query to get monthly tweets flagged for deletion

--- twitter-compliance-data/src/main/java/uk/ac/ucl/twitter/compliance/persist/TweetToVerifyMonthlyEntity.java
+++ twitter-compliance-data/src/main/java/uk/ac/ucl/twitter/compliance/persist/TweetToVerifyMonthlyEntity.java
@@ -1,7 +1,11 @@
 package uk.ac.ucl.twitter.compliance.persist;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.NamedQueries;
 import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 import java.io.Serializable;
@@ -12,13 +16,34 @@ import java.io.Serializable;
  */
 @Entity
 @Table(name = "PUBLIC.TWEET_TO_VALIDATE_MONTHLY")
-@NamedQuery(
-    name = "TweetToVerifyMonthlyEntity.findAll",
-    query = "SELECT t from TweetToVerifyMonthlyEntity t")
+@NamedQueries({
+  @NamedQuery(
+      name = "TweetToVerifyMonthlyEntity.findAll",
+      query = "SELECT t from TweetToVerifyMonthlyEntity t"),
+  @NamedQuery(
+      name = "TweetToVerifyMonthlyEntity.findAllToDelete",
+      query = "SELECT t from TweetToVerifyMonthlyEntity t "
+      + "INNER JOIN TweetToDeleteEntity d")
+})
 public class TweetToVerifyMonthlyEntity extends TweetToVerifyImpl
     implements TweetToVerify, Serializable {
 
   private static final long serialVersionUID = 1L;
+
+  /**
+   * Tweets flagged to be deleted.
+   */
+  @OneToOne
+  @JoinTable(
+    name = "PUBLIC.TWEET_TO_DELETE_MONTHLY",
+    joinColumns = {@JoinColumn(
+        name = "TWEET_TO_VALIDATE_MONTHLY_ID",
+        referencedColumnName = "tweetIdStr")},
+    inverseJoinColumns = {@JoinColumn(
+        name = "TWEET_TO_DELETE_ID",
+        referencedColumnName = "tweetIdStr")}
+  )
+  private TweetToDeleteEntity tweetToDelete;
 
   /**
    * Name of JPQL query. Selects all entities.
